88fd4031c3a5106c4e5fcd84c1ab1fca17067788
Annie Lydens <aelydens@gmail.com>
2015-06-19 11:43:18 -0600

    Prep for heroku

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
-require('./.env')
+process.env.DATABASE_URL || require('./.env')
 
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: process.env.PG_CONNECTION_STRING
+    connection: process.env.DATABASE_URL
   }
 };
